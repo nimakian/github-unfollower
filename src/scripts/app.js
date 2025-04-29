cb0df6ee48e27e88dff01a58470da6b0298c9b4c
nimakian0 <nimakianaghdam@gmail.com>
2025-04-29 16:55:47 +0330

    feat: add initial loading spinner

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -4,14 +4,24 @@ import {
 } from "../helpers/helper.js";
 
 
-const popupTokenElem = document.getElementById("popup-token")
-const openPopupTokenBtn = document.getElementById("open-popup-token")
-const hidePopupTokenBtn = document.getElementById("hide-popup-token")
-const popupTokenOverlay = document.getElementById("popup-token-overlay")
+window.addEventListener("load", () => {
+    const loaderElem = document.getElementById("loader")
+    setTimeout(() => {
+        loaderElem.classList.remove("flex")
+        loaderElem.classList.add("hidden")
+    }, 500); 
+    // Wait 0.5 seconds before hiding the loader to ensure it's visible for at least half a second
+
+    
+    const popupTokenElem = document.getElementById("popup-token")
+    const openPopupTokenBtn = document.getElementById("open-popup-token")
+    const hidePopupTokenBtn = document.getElementById("hide-popup-token")
+    const popupTokenOverlay = document.getElementById("popup-token-overlay")
 
 
 
-// Controls the showing and hiding of the popup
-openPopupTokenBtn.addEventListener("click", () => showPopup(popupTokenElem))
-hidePopupTokenBtn.addEventListener("click", () => hidePopup(popupTokenElem))
-popupTokenOverlay.addEventListener("click", () => hidePopup(popupTokenElem))
+    // Controls the showing and hiding of the popup
+    openPopupTokenBtn.addEventListener("click", () => showPopup(popupTokenElem))
+    hidePopupTokenBtn.addEventListener("click", () => hidePopup(popupTokenElem))
+    popupTokenOverlay.addEventListener("click", () => hidePopup(popupTokenElem))
+})
